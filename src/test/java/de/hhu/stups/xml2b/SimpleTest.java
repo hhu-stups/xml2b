fbf71c8d10d70bd4058e4ff5a42290e1edbbde04
Jan Gruteser <jan.gruteser@hhu.de>
2025-06-25 15:02:33 +0200

    update tests

--- src/test/java/de/hhu/stups/xml2b/SimpleTest.java
+++ src/test/java/de/hhu/stups/xml2b/SimpleTest.java
@@ -8,9 +8,11 @@ import de.prob.statespace.StateSpace;
 import de.prob.statespace.Transition;
 import org.junit.jupiter.api.*;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class SimpleTest {
 
@@ -35,9 +37,8 @@ public class SimpleTest {
 		State constants = stateSpace.getRoot().perform(Transition.SETUP_CONSTANTS_NAME);
 		Assertions.assertEquals("[rec(Element:\"order\",attributes:{(\"id\"↦XmlString(\"id_abc\"))},content:∅,maxCId:5,pIds:[0],recId:1,xmlLocation:(2↦20↦(9↦9))),rec(Element:\"customer\",attributes:{(\"name\"↦XmlString(\"Müller\"))},content:∅,maxCId:3,pIds:[1],recId:2,xmlLocation:(3↦29↦(5↦16))),rec(Element:\"item\",attributes:{(\"product\"↦XmlString(\"XMLFile1\")),(\"version\"↦XmlReal(1.0))},content:∅,maxCId:3,pIds:[1,2],recId:3,xmlLocation:(4↦49↦(4↦49))),rec(Element:\"customer\",attributes:{(\"name\"↦XmlString(\"Paul\"))},content:∅,maxCId:5,pIds:[1],recId:4,xmlLocation:(6↦27↦(8↦16))),rec(Element:\"item\",attributes:{(\"product\"↦XmlString(\"XMLFile2\"))},content:∅,maxCId:5,pIds:[1,4],recId:5,xmlLocation:(7↦35↦(7↦35)))]",
 				constants.eval("XML_DATA", FormulaExpand.EXPAND).toString());
-
-		pathMachine.toFile().deleteOnExit();
-		pathData.toFile().deleteOnExit();
+		Assertions.assertTrue(pathMachine.toFile().delete());
+		Assertions.assertTrue(pathData.toFile().delete());
 	}
 
 	@Test
@@ -49,10 +50,11 @@ public class SimpleTest {
 
 		XML2BCli.main(new String[]{pathInput.toFile().toString(),"-frw","NONE","-o",pathMachine.toString()});
 
-		Assertions.assertEquals(("[','(int(1),rec([field('Element',string(order)),field(attributes,[','(string(id),freeval('XML_ATTRIBUTE_TYPES','XmlString',string(id_abc))),','(string(id),freeval('XML_ATTRIBUTE_TYPES','XmlString',string(id_abc)))]),field(content,[]),field(maxCId,int(5)),field(pIds,[','(int(1),int(0))]),field(recId,int(1)),field(xmlLocation,','(','(int(2),int(20)),','(int(9),int(9))))])),','(int(2),rec([field('Element',string(customer)),field(attributes,[','(string(name),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('M\\374\\ller'))),','(string(name),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('M\\374\\ller')))]),field(content,[]),field(maxCId,int(3)),field(pIds,[','(int(1),int(1))]),field(recId,int(2)),field(xmlLocation,','(','(int(3),int(29)),','(int(5),int(16))))])),','(int(3),rec([field('Element',string(item)),field(attributes,[','(string(product),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('XMLFile1'))),','(string(version),freeval('XML_ATTRIBUTE_TYPES','XmlReal',term(floating(1.0)))),','(string(product),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('XMLFile1'))),','(string(version),freeval('XML_ATTRIBUTE_TYPES','XmlReal',term(floating(1.0))))]),field(content,[]),field(maxCId,int(3)),field(pIds,[','(int(1),int(1)),','(int(2),int(2))]),field(recId,int(3)),field(xmlLocation,','(','(int(4),int(49)),','(int(4),int(49))))])),','(int(4),rec([field('Element',string(customer)),field(attributes,[','(string(name),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('Paul'))),','(string(name),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('Paul')))]),field(content,[]),field(maxCId,int(5)),field(pIds,[','(int(1),int(1))]),field(recId,int(4)),field(xmlLocation,','(','(int(6),int(27)),','(int(8),int(16))))])),','(int(5),rec([field('Element',string(item)),field(attributes,[','(string(product),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('XMLFile2'))),','(string(product),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('XMLFile2')))]),field(content,[]),field(maxCId,int(5)),field(pIds,[','(int(1),int(1)),','(int(2),int(4))]),field(recId,int(5)),field(xmlLocation,','(','(int(7),int(35)),','(int(7),int(35))))]))].\n"), Files.readString(pathData));
-
-		pathMachine.toFile().deleteOnExit();
-		pathData.toFile().deleteOnExit();
+		List<String> lines = Files.readAllLines(pathData, StandardCharsets.UTF_8);
+		Assertions.assertFalse(lines.isEmpty(), "File is empty");
+		Assertions.assertEquals("[','(int(1),rec([field('Element',string(order)),field(attributes,[','(string(id),freeval('XML_ATTRIBUTE_TYPES','XmlString',string(id_abc))),','(string(id),freeval('XML_ATTRIBUTE_TYPES','XmlString',string(id_abc)))]),field(content,[]),field(maxCId,int(5)),field(pIds,[','(int(1),int(0))]),field(recId,int(1)),field(xmlLocation,','(','(int(2),int(20)),','(int(9),int(9))))])),','(int(2),rec([field('Element',string(customer)),field(attributes,[','(string(name),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('M\\374\\ller'))),','(string(name),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('M\\374\\ller')))]),field(content,[]),field(maxCId,int(3)),field(pIds,[','(int(1),int(1))]),field(recId,int(2)),field(xmlLocation,','(','(int(3),int(29)),','(int(5),int(16))))])),','(int(3),rec([field('Element',string(item)),field(attributes,[','(string(product),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('XMLFile1'))),','(string(version),freeval('XML_ATTRIBUTE_TYPES','XmlReal',term(floating(1.0)))),','(string(product),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('XMLFile1'))),','(string(version),freeval('XML_ATTRIBUTE_TYPES','XmlReal',term(floating(1.0))))]),field(content,[]),field(maxCId,int(3)),field(pIds,[','(int(1),int(1)),','(int(2),int(2))]),field(recId,int(3)),field(xmlLocation,','(','(int(4),int(49)),','(int(4),int(49))))])),','(int(4),rec([field('Element',string(customer)),field(attributes,[','(string(name),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('Paul'))),','(string(name),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('Paul')))]),field(content,[]),field(maxCId,int(5)),field(pIds,[','(int(1),int(1))]),field(recId,int(4)),field(xmlLocation,','(','(int(6),int(27)),','(int(8),int(16))))])),','(int(5),rec([field('Element',string(item)),field(attributes,[','(string(product),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('XMLFile2'))),','(string(product),freeval('XML_ATTRIBUTE_TYPES','XmlString',string('XMLFile2')))]),field(content,[]),field(maxCId,int(5)),field(pIds,[','(int(1),int(1)),','(int(2),int(4))]),field(recId,int(5)),field(xmlLocation,','(','(int(7),int(35)),','(int(7),int(35))))]))].", lines.get(0));
+		Assertions.assertTrue(pathMachine.toFile().delete());
+		Assertions.assertTrue(pathData.toFile().delete());
 	}
 
 	@Test
