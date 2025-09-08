e56bf5813437884ea9c9924f6487c04f81263f24
Jan Gruteser <jan.gruteser@hhu.de>
2025-09-08 07:20:39 +0200

    option to translate enum type attributes as string
    only for attributes that allow a combination with built-in types like REAL ito prevent type conflicts; option should be used if the data is not wrapped in a free type

--- src/main/java/de/hhu/stups/xml2b/bTypes/BEnumSetAttributeType.java
+++ src/main/java/de/hhu/stups/xml2b/bTypes/BEnumSetAttributeType.java
@@ -1,16 +1,28 @@
 package de.hhu.stups.xml2b.bTypes;
 
+import de.be4.classicalb.core.parser.node.AStringSetExpression;
 import de.be4.classicalb.core.parser.node.PExpression;
 import de.hhu.stups.xml2b.readXsd.TypeUtils;
 
 import static de.be4.classicalb.core.parser.util.ASTBuilder.createIdentifier;
+import static de.be4.classicalb.core.parser.util.ASTBuilder.createStringExpression;
 
 public class BEnumSetAttributeType extends BAttributeType {
 	private final BEnumSet enumSet;
+	private boolean requiresClassicalType = false;
 
 	public BEnumSetAttributeType(final String attributeName, final BEnumSet enumSet) {
 		super(attributeName, enumSet.getIdentifier());
 		this.enumSet = enumSet;
+	}
+
+	/**
+	 * Set to true, if raw expression is not used inside a free type and requires a classical B type.
+	 * Then, all expression for this attribute will be generated as a STRING to ensure correct typing.
+	 * Only effective if enumSet.isAllowBuiltIn is true, i.e. it also allows default types.
+	 */
+	public void setRequiresClassicalType(final boolean requiresClassicalType) {
+		this.requiresClassicalType = requiresClassicalType;
 	}
 
 	@Override
@@ -20,12 +32,17 @@ public class BEnumSetAttributeType extends BAttributeType {
 
 	@Override
 	public PExpression getSetExpression() {
+		if (enumSet.isAllowBuiltIn() && this.requiresClassicalType) {
+			return new AStringSetExpression();
+		}
 		return enumSet.getIdentifierExpression();
 	}
 
 	@Override
 	public PExpression getRawExpression(String data) {
 		if (enumSet.isAllowBuiltIn()) {
+			if (this.requiresClassicalType)
+				return createStringExpression(data);
 			BAttributeType type = TypeUtils.inferAttributeType(this.getAttributeName(), data);
 			return type.getRawExpression(data);
 		} else if (enumSet.isExtensible()) {
